812046991f22db53734ce062a975ca00d25829ec
yu1357465 <1035754866@qq.com>
2024-10-31 02:26:59 +0800

    feat: 增加进度条及优化样式与功能

--- wfd.py
+++ wfd.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 
 # 音频文件夹路径
 AUDIO_FOLDER = 'audio_files'
+PASSED_FOLDER = os.path.join(AUDIO_FOLDER, 'passed')
+
 @app.route('/')
 def index():
-    audio_files = [f for f in os.listdir('audio_files') if f.endswith('.mp3')]  # 替换为你的音频文件夹
+    audio_files = [
+        f for f in os.listdir(AUDIO_FOLDER) if f.endswith('.mp3')
+    ]
     audio_files.sort(key=lambda f: int(re.search(r'\d+', f).group()))  # 提取数字进行排序
     return render_template('index.html', audio_files=audio_files)
 
@@ -21,16 +25,14 @@ def audio(filename):
 def rename_and_move():
     old_name = request.form['old_filename']
     new_name = request.form['new_filename']
-    original_folder_path = r'C:\\Users\\ReiKa\\PycharmProjects\\WFDListening\\audio_files'
-    completed_folder_path = r'C:\\Users\\ReiKa\\PycharmProjects\\WFDListening\\audio_files\\passed'
 
-    old_path = os.path.join(original_folder_path, old_name)
-    new_path = os.path.join(completed_folder_path, new_name + '.mp3')
+    old_path = os.path.join(AUDIO_FOLDER, old_name)
+    new_path = os.path.join(PASSED_FOLDER, f"{new_name}.mp3")
+
+    if not os.path.exists(old_path):
+        return jsonify(success=False, message="源文件不存在")
 
     try:
-        if not os.path.exists(old_path):
-            return jsonify(success=False, message="源文件不存在")
-
         shutil.move(old_path, new_path)
         return jsonify(success=True, message="文件重命名并移动成功")
     except Exception as e:
@@ -38,19 +40,24 @@ def rename_and_move():
 
 @app.route('/get_audio_files', methods=['GET'])
 def get_audio_files():
-    audio_folder_path = 'audio_files'
-    files = [f for f in os.listdir(audio_folder_path) if f.endswith('.mp3')]
+    files = [f for f in os.listdir(AUDIO_FOLDER) if f.endswith('.mp3')]
     return jsonify(files)
+
+@app.route('/get_passed_file_count', methods=['GET'])
+def get_passed_file_count():
+    file_count = len([
+        f for f in os.listdir(PASSED_FOLDER) if f.endswith('.mp3')
+    ])
+    return jsonify(count=file_count)
 
 @app.route('/rename', methods=['POST'])
 def rename_file():
     old_filename = request.form['old_filename']
     new_filename = request.form['new_filename']
 
-    # 仅更改文件名，不更改后缀名
     if new_filename and old_filename:
-        old_filepath = os.path.join('audio_files', old_filename)  # 完整路径
-        new_filepath = os.path.join('audio_files', new_filename + '.mp3')  # 保持后缀名为 .mp3
+        old_filepath = os.path.join(AUDIO_FOLDER, old_filename)
+        new_filepath = os.path.join(AUDIO_FOLDER, f"{new_filename}.mp3")
 
         try:
             os.rename(old_filepath, new_filepath)
@@ -59,5 +66,4 @@ def rename_file():
             return jsonify(success=False, error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)  # 确保使用的端口与 start.py 中一致
+    app.run(debug=True, host='0.0.0.0', port=5000)  # 确保使用的端口与 start.py 中一致
